0d99e1372ead55c3c401cce29a28cdd9a62a93ba
OliveiraAlbertCoelho <albertoliveira@pursuit.org>
2019-10-15 17:09:36 -0400

    added favorites VC

---
+++ WeatherApp/WeatherApp/CustomCell/FavoriteCell.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteCell.swift
+//  WeatherApp
+//
+//  Created by albert coelho oliveira on 10/15/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UICollectionViewCell {
+    
+    lazy var favoriteImage: UIImageView = {
+        let favoriteImage = UIImageView()
+        return favoriteImage
+    }()
+}
---
+++ WeatherApp/WeatherApp/VC/FavoritesVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesVC.swift
+//  WeatherApp
+//
+//  Created by albert coelho oliveira on 10/15/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
